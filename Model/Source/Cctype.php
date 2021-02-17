6dd91ba7e08b3ee513dab28c29dc2e76f1ee9ff6
Yur <sergiy.yur@decimadigital.com>
2021-02-17 15:02:51 +0200

    Exclude deprecated dependency Magento\Authorizenet

---
+++ Model/Source/Cctype.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2021 TechNWeb, Inc. All rights reserved.
+ * See TNW_LICENSE.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace TNW\AuthorizeCim\Model\Source;
+
+use Magento\Payment\Model\Source\Cctype as PaymentCctype;
+
+/**
+ * Authorize.net Payment CC Types Source Model
+ *
+ * @package TNW\AuthorizeCim\Model\Source
+ */
+class Cctype extends PaymentCctype
+{
+    /**
+     * Return all supported credit card types
+     *
+     * @return string[]
+     */
+    public function getAllowedTypes()
+    {
+        return ['VI', 'MC', 'AE', 'DI', 'JCB', 'DN'];
+    }
+}
